6203de5c6008395bfd4ff5df86d1651669f59c46
klaustaj <klaustaj@rose-hulman.edu>
2017-11-27 16:27:52 -0500

    done

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -60,9 +60,12 @@ dave = rg.SimpleTurtle()
 # ----------------------------------------------------------------------
 # Ask the SimpleTurtle objects to do things:
 # ----------------------------------------------------------------------
+dave.speed = 5
 dave.forward(100)
 dave.left(90)
 dave.forward(200)
+dave.left(90)
+dave.forward(50)
 
 # ----------------------------------------------------------------------
 # Construct a new turtle and ask it to do things.
@@ -77,7 +80,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +94,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +112,17 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+jim = rg.SimpleTurtle()
+jim.speed = 10
+jim.right(90)
+jim.forward(50)
+jim.left(72)
+jim.forward(80)
+jim.right(72)
+jim.forward(100)
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
