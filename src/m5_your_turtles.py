e3fde2dfa68c833bb868f90c5fd09976c3470b32
klaustaj <klaustaj@rose-hulman.edu>
2017-11-27 17:00:20 -0500

    done

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Aaron Klaustermeier.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,26 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+jim = rg.SimpleTurtle()
+jim.pen = rg.Pen('red', 3)
+jim.speed = 10
+jim.left(30)
+
+jeff = rg.SimpleTurtle()
+jeff.pen = rg.Pen('green', 3)
+jeff.speed = 10
+jeff.left(60)
+
+for k in range(6):
+    jim.forward(50)
+    jim.left(60)
+
+for k in range(3):
+    jeff.forward(85)
+    jeff.left(120)
+
+window.close_on_mouse_click()
